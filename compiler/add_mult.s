bfa7c0a0dc094338e1790fe35ba34cb3d603791a
Jhouny <jhonyaraujo9121@gmail.com>
2026-03-09 10:12:54 +0100

    Added test cases for negative variables handling

--- compiler/add_mult.s
+++ compiler/add_mult.s
@@ -3,16 +3,28 @@
     pushq %rbp
     movq %rsp, %rbp
     subq $160, %rsp
-    movl $4, %eax
+    movl $2, %eax
     movl %eax, -4(%rbp)
-    movl $5, %eax
+    movl $3, %eax
+    movl %eax, -16(%rbp)
+    movl -4(%rbp), %eax
+    negl %eax
+    movl -16(%rbp), %edx
+    imull %edx, %eax
     movl %eax, -8(%rbp)
-    movl $6, %eax
-    movl -8(%rbp), %edx
+    movl -4(%rbp), %eax
+    negl %eax
+    movl %eax, -20(%rbp)
+    movl $3, %eax
+    movl %eax, -24(%rbp)
+    movl -8(%rbp), %eax
+    movl -24(%rbp), %edx
     addl %edx, %eax
     negl %eax
-    movl -4(%rbp), %edx
-    addl %edx, %eax
+    movl -20(%rbp), %edx
+    imull %edx, %eax
+    movl %eax, -12(%rbp)
+    movl -12(%rbp), %eax
     movq %rbp, %rsp
     popq %rbp
     ret 
---
+++ testfiles/18_negative_multiplication_variables.c
@@ -0,0 +1,7 @@
+int main() {
+   int a, b, c;
+   a = 2;
+   b = 3*-a;
+   c = -(a)*-(3+b);
+   return c; 
+}
